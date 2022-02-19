76c16aa26360290c1e57de1e5ca6df34d060b3a5
Han Liu <liuhan211211@gmail.com>
2022-02-19 16:49:49 +1100

    Turn sender to workers, apply factory methods

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,49 @@
+use anyhow::Result;
 use clap::Parser;
+use url::Url;
 
-mod sender;
+mod workers;
 
-/// A functional test tool for mini-bridge
+/// A speed and loss test for pass-through bridges between tcp, udp and serial port
 #[derive(Parser, Debug)]
-#[clap(author = "Han Liu <liuhan211211@gmail.com>", version, about, long_about = None)]
+#[clap(author = "Han Liu <liuhan211211@gmail.com>", version, about)]
 struct Args {
-    /// Input port
-    #[clap(short, long, required = true)]
-    input: String,
+    /// The output port in URL format.
+    /// Tcp client: "tcpclient://SERVER_ADDR:PORT".
+    /// Udp: "udp://SERVER_ADDR:PORT <LOCAL_ADDR:PORT>".
+    /// Serial port: "serial://SERIAL_PORT_DEVICE".
+    #[clap(
+        short = 'o',
+        long = "output",
+        required = true,
+        multiple_values = true,
+        max_values = 2
+    )]
+    output_port: Vec<Url>,
 
-    /// Output port
-    #[clap(short, long, required = true)]
-    output: String,
+    /// The input port in URL format.
+    /// Tcp client: "tcpclient://SERVER_ADDR:PORT".
+    /// Udp: "udp://SERVER_ADDR:PORT <LOCAL_ADDR:PORT>".
+    /// Serial port: "serial://SERIAL_PORT_DEVICE".
+    #[clap(
+        short = 'i',
+        long = "input",
+        required = true,
+        multiple_values = true,
+        max_values = 2
+    )]
+    input_port: Vec<Url>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
     println!(
-        "Test will send data to {} and listen to {}",
-        args.output, args.input
+        "Test will send data to {:?} and listen to {:?}",
+        args.input_port, args.output_port
     );
 
-    let sender = sender::create_sender(args.output).unwrap();
-    sender.output();
+    let output_worker = workers::create_worker(args.output_port)?;
+    output_worker.send();
+
+    Ok(())
 }
---
+++ src/workers.rs
@@ -0,0 +1,82 @@
+use anyhow::Context;
+use anyhow::{anyhow, Result};
+use std::net::SocketAddr;
+use std::net::ToSocketAddrs;
+use std::net::UdpSocket;
+use url::{Position, Url};
+
+pub trait Worker {
+    fn create(port: Vec<Url>) -> Result<Box<dyn Worker>>
+    where
+        Self: Sized;
+
+    fn send(&self);
+}
+
+struct UdpWorker {
+    dest: SocketAddr,
+    local: SocketAddr,
+    socket: UdpSocket,
+}
+
+impl Worker for UdpWorker {
+    fn create(port: Vec<Url>) -> Result<Box<(dyn Worker + 'static)>, anyhow::Error> {
+        let (socket, local) = if let Some(url) = port.get(1) {
+            let local = *&url[Position::BeforeHost..Position::AfterPath]
+                .to_socket_addrs()?
+                .next()
+                .ok_or(anyhow!("Local address invalid in udp worker"))?;
+            let socket = UdpSocket::bind(local).with_context(|| {
+                format!("Couldn't create udp worker with local address {}", local)
+            })?;
+            (socket, local)
+        } else {
+            let socket = UdpSocket::bind("0.0.0.0:0")
+                .with_context(|| "Couldn't create udp worker with random local port")?;
+            let local = socket.local_addr()?;
+            (socket, local)
+        };
+
+        let dest = *&port[0][Position::BeforeHost..Position::AfterPath]
+            .to_socket_addrs()?
+            .next()
+            .ok_or(anyhow!("Dest address invalid in udp worker"))?;
+
+        // Unlike in the TCP case, passing an array of addresses to the connect function
+        // of a UDP socket is not a useful thing to do: The OS will be unable to determine
+        // whether something is listening on the remote address without the application
+        // sending data.
+        socket.connect(dest).with_context(|| {
+            format!(
+                "Couldn't connect udp worker with local address {} to dest address {}",
+                local, dest
+            )
+        })?;
+
+        Ok(Box::new(UdpWorker {
+            dest,
+            local,
+            socket,
+        }))
+    }
+
+    fn send(&self) {
+        let buf = [0u8, 1, 2, 3];
+        self.socket.send(&buf).expect(
+            format!(
+                "Udp worker cannot send package from local address {} to dest address {}",
+                self.local, self.dest
+            )
+            .as_str(),
+        );
+    }
+}
+
+pub fn create_worker(port: Vec<Url>) -> Result<Box<dyn Worker>> {
+    match port[0].scheme() {
+        // "serial" => todo!(),
+        "udp" => UdpWorker::create(port),
+        // "tcpclient" => todo!(),
+        _ => Err(anyhow!(""))?,
+    }
+}
