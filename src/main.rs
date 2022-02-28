529e2ea1af1b30bda173c16ef29cceea82be712e
Han Liu <liuhan211211@gmail.com>
2022-02-28 22:13:34 +1100

    Update command line parser

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,48 @@ use url::Url;
 
 mod workers;
 
-/// A speed and loss test for pass-through bridges between tcp, udp and serial port
-#[derive(Parser, Debug)]
-#[clap(author = "Han Liu <liuhan211211@gmail.com>", version, about)]
-struct Args {
-    /// The output port in URL format.
-    /// Tcp client: "tcpclient://SERVER_ADDR:PORT".
-    /// Udp: "udp://SERVER_ADDR:PORT <LOCAL_ADDR:PORT>".
-    /// Serial port: "serial://SERIAL_PORT_DEVICE".
-    #[clap(
-        short = 'o',
-        long = "output",
-        required = true,
-        multiple_values = true,
-        max_values = 2
-    )]
-    output_port: Vec<Url>,
+fn url_validator(v: &str) -> Result<(), String> {
+    match Url::parse(v) {
+        Ok(u) => match u.scheme() {
+            "tcpclient" => Ok(()),
+            "udp" => Ok(()),
+            "serial" => Ok(()),
+            _ => Err(String::from("Url scheme not supported")),
+        },
+        Err(error) => Err(String::from("Url invalid: ") + &error.to_string()),
+    }
+}
 
-    /// The input port in URL format.
-    /// Tcp client: "tcpclient://SERVER_ADDR:PORT".
-    /// Udp: "udp://SERVER_ADDR:PORT <LOCAL_ADDR:PORT>".
-    /// Serial port: "serial://SERIAL_PORT_DEVICE".
-    #[clap(
-        short = 'i',
-        long = "input",
-        required = true,
-        multiple_values = true,
-        max_values = 2
-    )]
-    input_port: Vec<Url>,
+/// A speed and loss rate tester for transparent bridge between tcp, udp and serial port
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    /// Listening port for the testing data
+    #[clap(long = "listen-to", validator(url_validator))]
+    listen_to_port: Url,
+
+    /// Sending port for the testing data (required when using udp in "listen-to" port)
+    #[clap(long = "listen-from")]
+    listen_from_port: Option<Url>,
+
+    /// Destination of the testing data
+    #[clap(long = "send-to", validator(url_validator))]
+    send_to_port: Url,
+
+    /// Sending port for the testing data (required when using udp in "send-to" port)
+    #[clap(long = "send-from")]
+    send_from_port: Option<Url>,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
     println!(
-        "Test will send data to {:?} and listen to {:?}",
-        args.input_port, args.output_port
+        "Test will send data to {} and listen to {}",
+        args.send_to_port, args.listen_to_port
     );
 
-    let output_worker = workers::create_worker(args.output_port)?;
-    output_worker.send();
+    // let output_worker = workers::create_worker(args.output_port)?;
+    // output_worker.send();
 
     Ok(())
 }
