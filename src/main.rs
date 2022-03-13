3ebacf60e74fa6c105c9f0fe2af7893418cfcfbc
Han Liu <liuhan211211@gmail.com>
2022-03-13 21:11:48 +1100

    Add stats

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use clap::Parser;
-use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 mod serialworker;
 mod udpworker;
@@ -28,12 +27,12 @@ struct Args {
     sender_port: String,
 
     /// number of test packages
-    #[clap(long = "package-number", short = 'n', default_value = "100")]
+    #[clap(long = "package-number", short = 'n', default_value = "1000")]
     package_num: usize,
 
     /// length of each package
     #[clap(long = "package-length", short = 'p', default_value = "100")]
-    package_length: usize
+    package_length: usize,
 }
 
 fn main() -> Result<()> {
@@ -41,26 +40,37 @@ fn main() -> Result<()> {
     println!("listener: {}", args.listener_port);
     println!("sender: {}", args.sender_port);
 
-    let dispatcher_thread = thread::spawn(move || {
-        let sender = workers::create_worker(&args.sender_port).unwrap();
-        let listener = workers::create_worker(&args.listener_port).unwrap();
-        for i in 0..args.package_num {
-            let pkg = vec![i as u8; args.package_length];
-            // println!("[dispatcher_thread]  release pkg: {:?}", pkg.clone());
+    let sender = workers::create_worker(&args.sender_port).unwrap();
+    let listener = workers::create_worker(&args.listener_port).unwrap();
 
-            sender.send(pkg.clone(), None).unwrap();
-            let received = listener.receive(Some(Duration::from_millis(10))).unwrap();
+    println!("Testing...");
+    let start = Instant::now();
+    for i in 0..args.package_num {
+        let pkg = vec![i as u8; args.package_length];
+        // println!("[dispatcher_thread]  release pkg: {:?}", pkg.clone());
 
-            assert_eq!(received, pkg);
-            // println!("[dispatcher_thread] pkg verified");
+        sender.send(pkg.clone(), None).unwrap();
+        let received = listener.receive(Some(Duration::from_millis(10))).unwrap();
 
-            thread::sleep(Duration::from_millis(10));
-        }
-    });
-
-    dispatcher_thread
-        .join()
-        .expect("The dispatcher_thread thread has panicked");
+        assert_eq!(received, pkg);
+        // println!("[dispatcher_thread] pkg verified");
+    }
+    let duration = start.elapsed();
+    println!("Result:");
+    println!(
+        "   Transferred {} packages ({} bytes) in {} seconds",
+        args.package_num,
+        args.package_num * args.package_length,
+        duration.as_secs_f32()
+    );
+    println!(
+        "   Pacakges rate: {:.2} package per second",
+        args.package_num as f64 / duration.as_secs_f64()
+    );
+    println!(
+        "   Data rate: {:.2} byte per second",
+        args.package_num as f64 * args.package_length as f64 / duration.as_secs_f64()
+    );
 
     Ok(())
 }
