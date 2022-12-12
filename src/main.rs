a5d91794df1633f7e9bed8669bc304b8c5c7afc2
han.liu <liuhan211211@gmail.com>
2022-12-12 20:07:05 +1100

    Bi-directional comms

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use core::time;
 use std::{
     io::{Read, Write},
     net::TcpStream,
-    string, thread,
+    thread,
 };
 
 use signal_hook::{consts::SIGINT, iterator::Signals};
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result};
 use bus::Bus;
 use clap::{Arg, Command};
 
@@ -17,22 +17,22 @@ fn main() -> Result<()> {
         .author("Han.Liu, liuhan211211@gmail.com")
         .version(clap::crate_version!())
         .about("Speed tester for transparent transmission between tcp and serial port")
-        // .arg(
-        //     Arg::new("serial")
-        //         .required(true)
-        //         .short('s')
-        //         .long("serial")
-        //         .value_name("DEVICE:BAUD_RATE")
-        //         .help("Serial port device, for example: /dev/ttyUSB0:115200 (Linux) or COM1:115200 (Windows)"),
-        // )
-        // .arg(
-        //     Arg::new("tcp")
-        //         .short('t')
-        //         .required(true)
-        //         .long("tcp")
-        //         .value_name("ADDRESS:PORT")
-        //         .help("Tcp port, for example: 192.168.7.1:8000"),
-        // )
+        .arg(
+            Arg::new("serial")
+                // .required(true)
+                .short('s')
+                .long("serial")
+                .value_name("DEVICE:BAUD_RATE")
+                .help("Serial port device, for example: /dev/ttyUSB0:115200 (Linux) or COM1:115200 (Windows)"),
+        )
+        .arg(
+            Arg::new("tcp")
+                .short('t')
+                // .required(true)
+                .long("tcp")
+                .value_name("ADDRESS:PORT")
+                .help("Tcp port, for example: 192.168.7.1:8000"),
+        )
         .get_matches();
 
     let mut stop_signal: Bus<u8> = Bus::new(1);
@@ -59,11 +59,9 @@ fn main() -> Result<()> {
     let handle_tcp_tx = thread::spawn(move || {
         println!("tcp tx starts");
         loop {
-            if let Ok(size) = tcp_tx.write(b"test") {
-                // assert_eq!(size, 1);
-            } else {
-                assert!(false);
-            }
+            let n = tcp_tx.write(b"test").unwrap();
+            assert_eq!(n, 4);
+
             if stop_signal_reader_1.try_recv().is_ok() {
                 break;
             }
@@ -72,26 +70,22 @@ fn main() -> Result<()> {
         println!("tcp tx stopped");
     });
 
-    // // tcp rx
-    // thread::spawn(move || {
-    //     println!("tcp rx starts");
-    //     {
-    //         let mut buf = [0u8; 2048]; // max 2k
-    //         loop {
-    //             if let Ok(size) = tcp_rx.read(&mut buf) {
-    //                 for i in 0..size {
-    //                     assert_eq!(buf[i], 1); // validate
-    //                 }
-    //             } else {
-    //                 assert!(false); // error, should not goes here
-    //             }
-    //             if stop_signal_reader_2.try_recv().is_ok() {
-    //                 break;
-    //             }
-    //         }
-    //     }
-    //     println!("tcp rx stopped");
-    // });
+    // tcp rx
+    thread::spawn(move || {
+        let mut buf = [0u8; 2048]; // max 2k
+        println!("tcp rx starts");
+        loop {
+            if let Ok(n) = tcp_rx.read(&mut buf) {
+                println!("TCP Received: {:?}", std::str::from_utf8(&buf[..n]));
+            }
+
+            if stop_signal_reader_2.try_recv().is_ok() {
+                break;
+            }
+            thread::sleep(time::Duration::from_millis(10));
+        }
+        println!("tcp rx stopped");
+    });
 
     //#######################################################################################
     //                                        SERIAL
@@ -108,27 +102,28 @@ fn main() -> Result<()> {
     let mut serialport_tx = serialport.try_clone()?;
     let mut serialport_rx = serialport.try_clone()?;
 
-    // // serial tx
-    // thread::spawn(move || loop {
-    //     // if let Ok(pkg) = sender_rx.try_recv() {
-    //     //     serialport_tx.write(&pkg).unwrap();
-    //     // }
-    //     if stop_signal_reader_3.try_recv().is_ok() {
-    //         break;
-    //     }
-    // });
+    // serial tx
+    thread::spawn(move || {
+        println!("serial tx starts");
+        loop {
+            let n = serialport_tx.write(b"test").unwrap();
+            assert_eq!(n, 4);
+
+            if stop_signal_reader_3.try_recv().is_ok() {
+                break;
+            }
+            thread::sleep(time::Duration::from_millis(10));
+        }
+        println!("serial tx stopped");
+    });
 
     // serial rx
     let handle_serial_rx = thread::spawn(move || {
         let mut buf = [0u8; 2048]; // max 2k
-
         println!("serial rx starts");
         loop {
-            // if let Ok(size) = serialport_rx.read(&mut buf) {
-            //     receiver_tx.send(buf[..size].to_vec()).unwrap();
-            // }
             if let Ok(n) = serialport_rx.read(&mut buf[..]) {
-                println!("Received: {:?}", std::str::from_utf8(&buf[..n]));
+                println!("Serial Received: {:?}", std::str::from_utf8(&buf[..n]));
             }
 
             if stop_signal_reader_4.try_recv().is_ok() {
@@ -138,6 +133,8 @@ fn main() -> Result<()> {
         }
         println!("serial rx stopped");
     });
+
+    //#######################################################################################
 
     let mut signals = Signals::new(&[SIGINT])?;
     for sig in signals.forever() {
