301379c80c5669070664d59e6410c1c7ae3ac1cc
han.liu <liuhan211211@gmail.com>
2022-12-12 22:49:43 +1100

    Add unit test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use core::time;
 use std::{
     io::{Read, Write},
     net::TcpStream,
-    thread,
+    thread::{self},
 };
 
 use signal_hook::{consts::SIGINT, iterator::Signals};
@@ -10,6 +10,128 @@ use signal_hook::{consts::SIGINT, iterator::Signals};
 use anyhow::{Context, Result};
 use bus::Bus;
 use clap::{Arg, Command};
+
+struct TcpDevice {
+    stop_signal: Bus<u8>,
+}
+
+impl TcpDevice {
+    fn create(config: &str) -> Result<TcpDevice> {
+        let tcp = TcpStream::connect(config)
+            .with_context(|| format!("Failed to connect to remote_ip {}", config))?;
+        tcp.set_nodelay(true)?; // no write package grouping
+        tcp.set_write_timeout(None)?; // blocking write
+        tcp.set_read_timeout(None)?; // blocking read
+
+        let mut tcp_tx = tcp.try_clone()?;
+        let mut tcp_rx = tcp.try_clone()?;
+
+        let mut stop_signal: Bus<u8> = Bus::new(1);
+        let mut stop_tx = stop_signal.add_rx();
+        let mut stop_rx = stop_signal.add_rx();
+
+        // tcp tx
+        thread::spawn(move || {
+            println!("tcp tx starts");
+            loop {
+                let n = tcp_tx.write(b"test").unwrap();
+                assert_eq!(n, 4);
+
+                if stop_tx.try_recv().is_ok() {
+                    break;
+                }
+                thread::sleep(time::Duration::from_millis(10));
+            }
+            println!("tcp tx stopped");
+        });
+
+        // tcp rx
+        thread::spawn(move || {
+            let mut buf = [0u8; 2048]; // max 2k
+            println!("tcp rx starts");
+            loop {
+                if let Ok(n) = tcp_rx.read(&mut buf) {
+                    println!("TCP Received: {:?}", std::str::from_utf8(&buf[..n]));
+                }
+
+                if stop_rx.try_recv().is_ok() {
+                    break;
+                }
+                thread::sleep(time::Duration::from_millis(10));
+            }
+            println!("tcp rx stopped");
+        });
+
+        Ok(TcpDevice { stop_signal })
+    }
+
+    fn stop(&mut self) {
+        self.stop_signal.broadcast(0);
+    }
+}
+
+struct SerialDevice {
+    stop_signal: Bus<u8>,
+}
+
+impl SerialDevice {
+    fn create(config: &str) -> Result<SerialDevice> {
+        let mut serial_iter = config.split(':');
+        let device = serial_iter.next().unwrap();
+        let baud_rate = serial_iter.next().unwrap().parse::<u32>().unwrap();
+
+        let mut stop_signal: Bus<u8> = Bus::new(1);
+        let mut stop_tx = stop_signal.add_rx();
+        let mut stop_rx = stop_signal.add_rx();
+
+        let serialport = serialport::new(device, baud_rate).open().with_context(|| {
+            format!(
+                "Failed to open serialport device {} with baud rate {}",
+                device, baud_rate
+            )
+        })?;
+        let mut serialport_tx = serialport.try_clone()?;
+        let mut serialport_rx = serialport.try_clone()?;
+
+        // serial tx
+        thread::spawn(move || {
+            println!("serial tx starts");
+            loop {
+                let n = serialport_tx.write(b"test").unwrap();
+                assert_eq!(n, 4);
+
+                if stop_tx.try_recv().is_ok() {
+                    break;
+                }
+                thread::sleep(time::Duration::from_millis(10));
+            }
+            println!("serial tx stopped");
+        });
+
+        // serial rx
+        thread::spawn(move || {
+            let mut buf = [0u8; 2048]; // max 2k
+            println!("serial rx starts");
+            loop {
+                if let Ok(n) = serialport_rx.read(&mut buf[..]) {
+                    println!("Serial Received: {:?}", std::str::from_utf8(&buf[..n]));
+                }
+
+                if stop_rx.try_recv().is_ok() {
+                    break;
+                }
+                thread::sleep(time::Duration::from_millis(10));
+            }
+            println!("serial rx stopped");
+        });
+
+        Ok(SerialDevice { stop_signal })
+    }
+
+    fn stop(&mut self) {
+        self.stop_signal.broadcast(0);
+    }
+}
 
 fn main() -> Result<()> {
     let m = Command::new("ser2tcp-tester")
@@ -33,129 +155,40 @@ fn main() -> Result<()> {
         )
         .get_matches();
 
-    let mut stop_signal: Bus<u8> = Bus::new(1);
-    let mut stop_signal_reader_1 = stop_signal.add_rx();
-    let mut stop_signal_reader_2 = stop_signal.add_rx();
-    let mut stop_signal_reader_3 = stop_signal.add_rx();
-    let mut stop_signal_reader_4 = stop_signal.add_rx();
-
-    let tcp_addr = m.get_one::<String>("tcp").unwrap();
-    let mut serial_iter = m.get_one::<String>("serial").unwrap().split(':');
-    let serial_device = serial_iter.next().unwrap();
-    let serial_baud_rate = serial_iter.next().unwrap().parse::<u32>().unwrap();
-
-    println!("tcp_addr: {:?}", tcp_addr);
-    println!(
-        "serial_device: {:?}, serial_baud_rate: {:?}",
-        serial_device, serial_baud_rate
-    );
-
-    //#######################################################################################
-    //                                        TCP
-    //#######################################################################################
-
-    let tcp = TcpStream::connect(tcp_addr)
-        .with_context(|| format!("Failed to connect to remote_ip {}", tcp_addr))?;
-    tcp.set_nodelay(true)?; // no write package grouping
-    tcp.set_write_timeout(None)?; // blocking write
-    tcp.set_read_timeout(None)?; // blocking read
-
-    let mut tcp_tx = tcp.try_clone()?;
-    let mut tcp_rx = tcp.try_clone()?;
-
-    // tcp tx
-    let handle_tcp_tx = thread::spawn(move || {
-        println!("tcp tx starts");
-        loop {
-            let n = tcp_tx.write(b"test").unwrap();
-            assert_eq!(n, 4);
-
-            if stop_signal_reader_1.try_recv().is_ok() {
-                break;
-            }
-            thread::sleep(time::Duration::from_millis(10));
-        }
-        println!("tcp tx stopped");
-    });
-
-    // tcp rx
-    thread::spawn(move || {
-        let mut buf = [0u8; 2048]; // max 2k
-        println!("tcp rx starts");
-        loop {
-            if let Ok(n) = tcp_rx.read(&mut buf) {
-                println!("TCP Received: {:?}", std::str::from_utf8(&buf[..n]));
-            }
-
-            if stop_signal_reader_2.try_recv().is_ok() {
-                break;
-            }
-            thread::sleep(time::Duration::from_millis(10));
-        }
-        println!("tcp rx stopped");
-    });
-
-    //#######################################################################################
-    //                                        SERIAL
-    //#######################################################################################
-
-    let serialport = serialport::new(serial_device, serial_baud_rate)
-        .open()
-        .with_context(|| {
-            format!(
-                "Failed to open serialport device {} with baud rate {}",
-                serial_device, serial_baud_rate
-            )
-        })?;
-    let mut serialport_tx = serialport.try_clone()?;
-    let mut serialport_rx = serialport.try_clone()?;
-
-    // serial tx
-    thread::spawn(move || {
-        println!("serial tx starts");
-        loop {
-            let n = serialport_tx.write(b"test").unwrap();
-            assert_eq!(n, 4);
-
-            if stop_signal_reader_3.try_recv().is_ok() {
-                break;
-            }
-            thread::sleep(time::Duration::from_millis(10));
-        }
-        println!("serial tx stopped");
-    });
-
-    // serial rx
-    let handle_serial_rx = thread::spawn(move || {
-        let mut buf = [0u8; 2048]; // max 2k
-        println!("serial rx starts");
-        loop {
-            if let Ok(n) = serialport_rx.read(&mut buf[..]) {
-                println!("Serial Received: {:?}", std::str::from_utf8(&buf[..n]));
-            }
-
-            if stop_signal_reader_4.try_recv().is_ok() {
-                break;
-            }
-            thread::sleep(time::Duration::from_millis(10));
-        }
-        println!("serial rx stopped");
-    });
-
-    //#######################################################################################
+    let mut tcp_device =
+        TcpDevice::create(m.get_one::<String>("tcp").expect("tcp config is required"))?;
+    let mut serial_device = SerialDevice::create(
+        m.get_one::<String>("serial")
+            .expect("serial config is required"),
+    )?;
 
     let mut signals = Signals::new(&[SIGINT])?;
-    for sig in signals.forever() {
-        println!("\n===== Received signal {:?} =====", sig);
-        break;
-    }
+    signals.wait();
 
-    stop_signal.broadcast(0);
-    handle_tcp_tx.join().unwrap();
-    handle_serial_rx.join().unwrap();
+    tcp_device.stop();
+    serial_device.stop();
 
     Ok(())
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use core::time;
+    use std::thread;
+
+    use crate::{SerialDevice, TcpDevice};
+
+    #[test]
+    fn test_serial_device() {
+        let mut dev = SerialDevice::create("/tmp/serial2:115200").unwrap();
+        thread::sleep(time::Duration::from_millis(2000));
+        dev.stop();
+    }
+
+    #[test]
+    fn test_tcp_device() {
+        let mut dev = TcpDevice::create("127.0.0.1:2000").unwrap();
+        thread::sleep(time::Duration::from_millis(2000));
+        dev.stop();
+    }
+}
