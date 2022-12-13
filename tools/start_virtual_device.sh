87d9341caa3b5858970970b4705fad1cd05e9947
han.liu <liuhan211211@gmail.com>
2022-12-13 18:26:33 +1100

    Improve virtual device

--- tools/start_virtual_device.sh
+++ tools/start_virtual_device.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 socat -dd pty,raw,echo=0,link=/tmp/serial1 pty,raw,echo=0,link=/tmp/serial2 &
-ser2net -d -C 2000:raw:0:/tmp/serial1:115200 &
-# socat - /tmp/serial2
+ser2net -C 2000:raw:0:/tmp/serial1:115200
